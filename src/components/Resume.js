19af92b3f35c76fe49e4930717f69b9c96808627
Sam Avery <sam.avery1@gmail.com>
2023-01-18 12:06:00 -0500

    small copy edit in Bio

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,10 @@
-import resume from '../images/pretty-resume-2.png'
+// import resume from '../images/pretty-resume-2.png'
 
 
 function Resume() {
     return (
         <div className='resume'>
-            <img src={resume}/>
+            <img src="https://i.imgur.com/VSS9OdX.png"/>
         </div>
     );
   }
