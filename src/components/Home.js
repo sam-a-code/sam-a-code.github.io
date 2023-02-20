1ff95623e2d4510b0987f466a729af5f63164d4f
Sam Avery <sam.avery1@gmail.com>
2023-02-20 10:11:00 -0600

    reordering homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,9 +8,9 @@ function Home() {
     return (
         <>
             <Bio />
+            <HomePageProjects />
             <JobCardContainer />
             <EducationCardContainer />
-            <HomePageProjects />
         </>
 
     );
