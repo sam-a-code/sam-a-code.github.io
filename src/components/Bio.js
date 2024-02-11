f8b792715784c4f5263c180165cf327e809824b1
Sam Avery <107422193+sam-a-code@users.noreply.github.com>
2024-02-11 11:36:04 -0600

    Update Bio.js

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,9 +6,9 @@ function Bio() {
             <div className='bio-parent'>
                 <div className='bio-child'>
                     <h2>Who I am</h2>
-                    <p>I'm Sam – lover of farmers markets, creative pursuits, bad puns, and the em dash. I believe it's important to leave our campsites cleaner than we found them (despite not being very outdoorsy, I do love this sentiment). The most popular spreadsheet I've ever created is one for restaurants – new ones I want to try, ones I recommend for out of town guests, dishes you simply MUST order, etc. You can filter by brunch and patio availability, obviously.</p>
+                    <p>I'm Sam – lover of farmers markets, creative pursuits, bad puns, and the em dash. I believe it's important to leave our campsites cleaner than we found them. The most popular spreadsheet I've ever created is one for restaurants – new ones I want to try, ones I recommend for out of town guests, dishes you can't miss, etc. You can filter by brunch and patio availability, obviously.</p>
                     <p>Throughout my journey I've collected a pretty wide array of experiences. From program managing a social impact business conference, to wearing a ton of hats at a startup focused on creative fulfillment, to graduating from a software engineering bootcamp and building this website (the one you're on right now!), my experience has always been focused on learning, creativity, and social impact. </p>
-                    <p>I'm inspired by businesses who are focused on making the world a better place and I'm driven by work that helps people accomplish their creative goals. I also draw a lot – check out some of my designs on the art tab.</p>
+                    <p>I'm inspired by businesses who are focused on making the world a better place and I'm driven by work that helps people accomplish their creative goals. I also draw a lot – check out some of my designs on the art tab. I'm relaunching my design website soon, so keep an eye out!</p>
                 </div>
                 <img src='https://i.imgur.com/aVT48B4.jpg' alt="Sam's headshot, wearing a dark green dress in front of a wall of greenery" className='bio-child'/>
             </div>
@@ -16,18 +16,16 @@ function Bio() {
                 <img src='https://i.imgur.com/KXhYLW1.png' alt="green layers of abstract shapes" className='bio-child'/>
                 <div className='bio-child'>
                     <h2>What I do</h2>
-                    <p>Currently, what I'm doing is looking for a job! The roles in which I'm most interested are: Program Manager, Product Manager, or Software Developer.</p>
+                    <p>Currently, I'm working in Operations at a fintech company and loving it.</p>
                     <p>I started out working in events at a production facility, hosting music nights, film premieres, workshops, album launches, and more. After a few years, I was promoted to the Operations team, supporting the global network of production sites. After about five years at that company, I decided it was time for a change. I quit my job (leaving a team I loved dearly), put almost all of my possessions in storage, and drove across the country.</p>
                     <p>I joined a startup that offered e-learning experiences for creatives. While there, I realized that I could be significantly more self-sufficient if I improved my technical skills. I learned how to update our codebase with new class launch content – even implementing some bug fixes! It was empowering to be able to make changes myself, and that gave me the push I needed to sign up for a coding bootcamp.</p>
-                    <p>The bootcamp was one of the most challenging things I've undertaken in a long time, but I'm so glad I did it. It felt good to challenge myself and to bet on myself – that this leap of faith would pay off. We learned React.js (my favorite), Ruby on Rails, CSS, JavaScript, SQL, and more. Click over to my coding portfolio to see some of the projects I've created!</p>
+                    <p>The bootcamp was one of the most challenging things I've undertaken in a long time, but I'm so glad I did it. It felt good to challenge myself and to bet on myself – that this leap of faith would pay off. We learned React.js (my favorite), Ruby on Rails, CSS, JavaScript, SQL, and more.</p>
                 </div>
             </div>
             <div className='bio-parent'>
                 <div className='bio-child'>
                     <h2>Why I do it</h2>
-                    <p>I think the world is so much more vibrant with art and creativity and connection – and I can't wait to see how I can incorporate those into my next role.</p>
-                    <p>I excel at building cross-functional relationships and communicating between different teams. I love the challenges of program management – balancing requests from multiple stakeholders, creating and improving processes, and ensuring all deliverables are fulfilled on time and with a fantastic attitude. I love learning the ins and outs of new tools and I find satisfaction in making processes more efficient.</p>
-                    <p>I'm eager to implement my new software engineering skills wherever I can. The ability to build new features, improve user journeys from a process and UX perspective, and to continue improving my technical expertise makes me really excited for whatever comes next.</p>
+                    <p>I think the world is so much more vibrant with art and creativity and connection – and I try to incorporate each of them into my life every day.</p>
                     </div>
                     <img src='https://i.imgur.com/hAJVs9N.png' alt="a screenshot of the homepage of Sam's dinner party app" className='bio-child'/>
             </div>
